0edbdd8283e44ecc30d406b9544557513f0f077d
Dieu Nguyen <dieunguyencva@gmail.com>
2025-11-10 15:46:15 +0700

    Update Test

--- lms_be/src/main/java/edu/lms/config/SecurityConfig.java
+++ lms_be/src/main/java/edu/lms/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                         .configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/api/login", "/api/register", "/api/v1/public/**", "/api/lessons/**", "/api/news-articles", "/api/refresh").permitAll()
+                        .requestMatchers("/api/login", "/api/register", "/api/v1/public/courses/**", "/api/lessons/**", "/api/news-articles", "/api/refresh").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
--- lms_be/src/test/java/edu/lms/controller/CourseControllerTest.java
+++ lms_be/src/test/java/edu/lms/controller/CourseControllerTest.java
@@ -1,7 +1,9 @@
 package edu.lms.controller;
 
 import edu.lms.dto.CourseResponse;
+import edu.lms.security.JwtAuthenticationFilter;
 import edu.lms.security.JwtService;
+import edu.lms.security.RestAuthenticationEntryPoint;
 import edu.lms.service.CourseService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,11 +23,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CourseController.class)
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 class CourseControllerTest {
 
     @Autowired
@@ -35,13 +39,20 @@ class CourseControllerTest {
     @MockitoBean
     private JwtService jwtService;
 
+    @MockitoBean
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @MockitoBean
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
     @Test
     @DisplayName("GET /public/courses - success with searchKeyword")
     void testGetCoursesWithKeyword() throws Exception {
+
         // GIVEN: mock data
         List<CourseResponse> mockCourses = List.of(
-                new CourseResponse(1L, "Java Spring Boot", "Backend", "Msr. Dieu"),
-                new CourseResponse(2L, "React Fundamentals", "Frontend", "Ms. Lan")
+                new CourseResponse(1L, "SBA301", "Java Spring Boot", "Msr. Dieu"),
+                new CourseResponse(2L, "RCT210","React Fundamentals", "Ms. Lan")
         );
 
         Page<CourseResponse> mockPage = new PageImpl<>(mockCourses);
@@ -50,12 +61,13 @@ class CourseControllerTest {
                 .thenReturn(mockPage);
 
         // WHEN & THEN: perform request
-        mockMvc.perform(MockMvcRequestBuilders.get("/public/courses")
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/public/courses")
                         .param("searchKeyword", "java")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].title").value("Java Spring Boot"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].instructor").value("Ms. Lan"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].courseCode").value("SBA301"));
+//                .andExpect(MockMvcResultMatchers.jsonPath("$[0].lecturer").value("Msr. Dieu"));
 
         Mockito.verify(courseService, Mockito.times(1))
                 .findByKeyword("java", 0, 20);
